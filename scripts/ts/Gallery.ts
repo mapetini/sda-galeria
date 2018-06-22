e2e39ee2e80e17757bdf056a1e0588ac346ab2bb
[Mateusz] <[mhalman094@gmail.com]>
2018-06-22 18:49:00 +0200

    Temporary commit with css changes

--- scripts/ts/Gallery.ts
+++ scripts/ts/Gallery.ts
@@ -6,23 +6,24 @@ export class Gallery {
     private imgTitle: Element;
     private mainImg: Element;
     private description: Element;
-    private thumbnail: Element;
+    private thumbnailContainer: Element;
     private counter: number = 0;
     
     constructor(private photos: Photo[], element: Element) {
+        this.initElements(element);
+        this.next.addEventListener("click", () => this.showNextPhoto());
+        this.prev.addEventListener("click", () => this.showPrevPhoto());
+        this.updateMainPhoto();
+        this.updateThumbnails();
+    }
+
+    initElements(element: Element) {
         this.prev = element.getElementsByClassName("btn-prev")[0];
         this.next = element.getElementsByClassName("btn-next")[0];
         this.imgTitle = element.getElementsByTagName("h1")[0];
         this.mainImg = element.getElementsByTagName("img")[0];
         this.description = element.getElementsByTagName("p")[0];
-        this.thumbnail = element.getElementsByClassName("thumb-container")[0];
-        this.next.addEventListener("click", () => this.showNextPhoto());
-        this.prev.addEventListener("click", () => this.showPrevPhoto());
-        this.mainImg.style.width = '50vw';
-        this.updateMainPhoto();
-        this.putThumbnails();
-        console.log(this.thumbnail);
-
+        this.thumbnailContainer = element.getElementsByClassName("thumbs-container")[0];
     }
     updateMainPhoto() {
         const photo = this.photos[this.counter];
@@ -45,7 +46,11 @@ export class Gallery {
         this.updateMainPhoto();
     }
 
-    putThumbnails() {
-        
+    updateThumbnails() {
+        let div: string = '';
+        for (let i=0; i < this.photos.length; i++){
+            div += `<img src=${this.photos[i].source}>`;
+        }
+        this.thumbnailContainer.innerHTML = div;
     }
 }
